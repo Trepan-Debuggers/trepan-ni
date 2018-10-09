f9ab16afde555de497d311c9a6817ae75a1b89dc
rocky <rb@dustyfeet.com>
2018-10-09 16:38:09 -0400

    Add "--help" and --inspect" options
    --inspect: don't stop initially

--- test/cli/invalid-args.test.js
+++ test/cli/invalid-args.test.js
@@ -11,7 +11,8 @@ test('launch CLI w/o args', (t) => {
   return cli.quit()
     .then((code) => {
       t.equal(code, 1, 'exits with non-zero exit code');
-      t.match(cli.output, /^Usage:/, 'Prints usage info');
+      t.match(cli.output, /You need to give at least/,
+              'Prints usage error message');
     });
 });
 
